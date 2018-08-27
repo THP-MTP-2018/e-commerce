55d112b148c880201aca9cffa5fd245c708dec2c
badr96 <badreddine.machmachi@efrei.net>
2018-08-27 19:21:46 +0200

    seed still doesn't work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,63 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# 20.times do
+#   User.create(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     email: Faker::Internet.email,
+#     encrypted_password: Faker::Name.last_name,
+#     adress: Faker::Address.street_address,
+#     zip_code: Faker::Address.zip_code,
+#     city: Faker::Address.city,
+#     phone_number: Faker::PhoneNumber.phone_number,
+#     product_id: Product.all.sample.id
+#   )
+# end
+
+20.times do
+	Product.create(
+		name_article: "Maillot",
+		img: "https://cdn.vox-cdn.com/thumbor/6x2xbMVb75Lefx-ZusHzwttXR_c=/0x0:1280x960/920x613/filters:focal(538x378:742x582):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/57601275/60861120_1280x960.0.0.jpg",
+		price: '24.00',
+		category_id:  '1',
+		promotion_id: '1'
+	)
+end
+
+# 20.times do
+# 	Category.create(name: Faker::OnePiece.island)
+# end
+
+# 20.times do
+# 	Promotion.create(
+# 		code: Faker::DragonBall.character,
+# 		percentage: '25',
+# 		admin_id: Admin.all.sample.id
+# 		)
+# end
+
+# 20.times do
+# 	Invoice.create(
+# 		user_id: User.all.sample.id,
+# 		product_id: Product.all.sample.id,
+# 		final_price: '13'		
+# 		)
+# end
+
+# 20.times do
+# 	Order.create(
+# 		user_id: User.all.sample.id,
+# 		invoice_id: Invoice.all.sample.id,
+# 		product_id: Product.all.sample.id,
+# 		)
+# end
+
+# 5.times do
+# 	Admin.create(
+# 		first_name: Faker::Name.first_name,
+#     	last_name: Faker::Name.last_name,
+#     	email: Faker::Internet.email,
+#     	encrypted_password: Faker::Name.last_name
+# 		)
+# end
+
