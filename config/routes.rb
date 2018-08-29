5c3911c5415c6da7fcf4c7e9c0bbd28161327d79
jean <nguyen.jean@me.com>
2018-08-29 19:29:23 +0200

    style add

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
 
  resources :products
   devise_for :admins
-  devise_for :users
-
-
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
 
   root 'home#index'
   get 'cart', to: 'home#cart'
-  get 'categories', to: 'home#categories'
+  get 'produits', to: 'home#categories'
   get 'checkout', to: 'home#checkout'
   get 'contact', to: 'home#contact'
   get 'dashboard', to: 'admin#dashboard'
